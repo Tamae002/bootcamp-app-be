9181e33edfaec2fc3941b227d1c817bd42f7a148
M. Arvio <arvizior@gmail.com>
2025-11-29 18:15:13 +0700

    Welcome

--- src/routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { login } from '../controllers/auth/login.controller.js'
-import { logout } from '../controllers/auth/logout.controller.js'
-import { isAuthenticated } from '../middlewares/isAuthenticated.middleware.js'
-
-export const authRoutes = express.Router()
-
-authRoutes.post('/login', login)
-authRoutes.get('/logout', logout)
