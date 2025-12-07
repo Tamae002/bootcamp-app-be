3dc5747fb44721b458ed1ceb84df633c262b701d
M. Arvio <arvizior@gmail.com>
2025-12-07 16:15:05 +0700

    all example vanish

--- src/routes/testRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const testController = require("../controllers/testController");
-
-router.get("/users", testController.getAllUsers);
-router.get("/kelas", testController.getAllKelas);
-router.get("/pertemuan/:kelasId", testController.getPertemuanByKelas);
-
-module.exports = router;
