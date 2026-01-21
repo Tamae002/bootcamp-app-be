4dc56a1ebdfd8a8bb4ea442abde759757684b684
Andhika Dwitama <andhikaDWA@gmail.com>
2026-01-21 11:49:32 +0700

    fix folder file missing

--- src/middleware/file.middleware.js
+++ src/middleware/file.middleware.js
@@ -3,14 +3,14 @@ import path from 'path';
 import fs from 'fs';
 import crypto from 'crypto';
 
-const uploadDir = 'file';
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-  console.log(`📁 Folder '${uploadDir}' berhasil dibuat.`); // ← LOG INI HARUS MUNCUL
-}
+const uploadDir = path.join(process.cwd(), "file");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir, { recursive: true });
+      console.log(`📁 Folder '${uploadDir}' berhasil dibuat.`); // ← LOG INI HARUS MUNCUL
+    }
     cb(null, uploadDir);
   },
   filename: (req, file, cb) => {
