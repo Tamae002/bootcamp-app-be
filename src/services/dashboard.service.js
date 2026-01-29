81fef8263dfdd347547b3e99c47b3ec657695f78
M. Arvio <arvizior@gmail.com>
2026-01-29 12:39:17 +0700

    Dashboard Fixes

--- src/services/dashboard.service.js
+++ src/services/dashboard.service.js
@@ -3,7 +3,7 @@ import prisma from '../config/prisma.js';
 export async function getDashboardStats() {
   const now = new Date();
 
-  const [jumlahPeserta, jumlahMentor, jumlahKelas, jumlahPertemuanMendatang, kelasAktif] =
+  const [jumlahPeserta, jumlahMentor, jumlahPertemuan, jumlahKelas, kelasAktif] =
     await Promise.all([
       prisma.user.count({
         where: {
@@ -11,8 +11,9 @@ export async function getDashboardStats() {
           isActive: true,
         },
       }),
-      prisma.kelas.count({
+      prisma.user.count({
         where: {
+          role: 'user',
           isActive: true,
         },
       }),
@@ -24,6 +25,11 @@ export async function getDashboardStats() {
           isActive: true,
         },
       }),
+      prisma.kelas.count({
+        where: {
+          isActive: true
+        }
+      }),
       prisma.kelas.findMany({
         where: {
           isActive: true,
@@ -32,15 +38,26 @@ export async function getDashboardStats() {
         select: {
           kelas_id: true,
           nama_kelas: true,
+          _count: {
+            select: {
+              anggota: true, // Menghitung jumlah record di anggota_kelas
+            },
+          },
         },
       }),
     ]);
+
+ const formattedKelasAktif = kelasAktif.map((kelas) => ({
+    kelas_id: kelas.kelas_id,
+    nama_kelas: kelas.nama_kelas,
+    total_peserta: kelas._count.anggota, // Total peserta dari relasi anggota_kelas
+  }));
 
   return {
     jumlah_peserta: jumlahPeserta,
     jumlah_mentor: jumlahMentor,
     jumlah_kelas: jumlahKelas,
-    jumlah_pertemuan_mendatang: jumlahPertemuanMendatang,
-    kelas_aktif: kelasAktif,
+    jumlah_pertemuan: jumlahPertemuan,
+    kelas_aktif: formattedKelasAktif,
   };
 }
